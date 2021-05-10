c476e489948f9172af433530a564d15bbe5cda52
aliou <51207@etu.he2b.be>
2021-05-10 19:01:56 +0200

    seveur1_maj1

--- serveur_maj1.cs
+++ serveur_maj1.cs
@@ -1,106 +1,81 @@
- class Program
+class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
 
             Socket listener;
-          
             IPAddress ip = IPAddress.Parse("192.168.1.5");
-
-           //connecting player1
             int port = 8001;
             IPEndPoint localip = new IPEndPoint(ip, port);
             listener = new Socket(SocketType.Stream, ProtocolType.Tcp);
             listener.Bind(localip);
             listener.Listen(100);
-            Console.WriteLine("waiting connexion client 1 ......");
+            Console.WriteLine("waiting connexion ......");
             int n = 1;
-            Socket client1 = listener.Accept();
-            Socket client2 = listener.Accept();
-            Console.WriteLine("accept client1");
-        
-
-            byte[] buffer1 = Encoding.ASCII.GetBytes("ID:0");
-            byte[] buffer2 = Encoding.ASCII.GetBytes("ID:1");
-
-            client1.Send(buffer2);
-            client2.Send(buffer1);
-
-
-
+            Socket client = listener.Accept();
+            Console.WriteLine("accept client");
             while (true)
             {
-         
-                
-                
-                // reception     
+          
+          
+          # reception     
           
                 byte[] buf2 = new byte[1024];
-                int bytecode = client1.Receive(buf2);
-                string msg1 = Encoding.ASCII.GetString(buf2, 0, bytecode);
- 
-                buf2 = new byte[1024];
-               bytecode = client2.Receive(buf2);
-                string msg2 = Encoding.ASCII.GetString(buf2, 0, bytecode);
-
-
-                string [] data =  msg1.Split('/');
+                int bytecode = client.Receive(buf2);
+                string msg = Encoding.ASCII.GetString(buf2, 0, bytecode);
+            
+                    
+                    
                    
-                //Split: Returns a string array that contains the substrings in this instance that are delimited by elements of a specified string or Unicode character array.
-            
-                    string movement = data[0];
+        string [] data =  msg.Split('/');
+        
+     
+     #Split: Returns a string array that contains the substrings in this #instance that are delimited by elements of a specified #string or #Unicode character array.
+         
+
+
+         string movement = data[0];
    
-                    string plateform = data[1];
+         string plateform = data[1];
             
-                    string coina = data[2];
+         string coina = data[2];
  
-                    string scoring = data[3];
+         string scoring = data[3];
                
-                    string[] moov = movement.Split(':');
+         string[] moov = movement.Split(':');
               
-                    string[] piece = coina.Split(':');
+         string[] piece = coina.Split(':');
 
          
-                    if (plateform == "1") { Console.WriteLine("plateforme ok"); }
                 
-             
-                      
-                   if(piece[0] != "-1") { Console.WriteLine("le joueur a pris un coin: a" ); }
+         //*** plateform ***
+                
+         if (plateform == "1") { Console.WriteLine("plateforme ok"); }
+                
+        
+                
+         //*** coin *** 
          
-                    
-                    if (piece[1] != "-1") { Console.WriteLine(" le joueur a pris un coin : b"); }
+             if(piece[2] == "1") { Console.WriteLine("le coin est 1" ); }
          
-                    
-                    
+             if (piece[2] == "2") { Console.WriteLine("le coin est 2"); }
+         
+             if (piece[2] == "3") { Console.WriteLine("le coin est 3"); }
             
              
+                
+                
+                
+         //*** envoie de données: ***
+
+             byte[] buffer = Encoding.ASCII.GetBytes(msg);
 
 
-                if (moov[0] == "1")
-                {
-                    Console.WriteLine(" left player");
+            client.Send(buffer);
 
-                }
-                    else if (moov[1] == "1"){ Console.WriteLine(" Right player"); }
-              
-                
-                // Console.WriteLine(moov[2]);
-                if(moov[2] == "0") { Console.WriteLine(" No jump "); }
-                
-                
-                if (moov[2] == "1") { Console.WriteLine(" jump and falling "); }
-                
-                
-                if (moov[2] == "2") { Console.WriteLine(" he can jump  "); }
-             
-              buffer1 = Encoding.ASCII.GetBytes(msg1);
-              buffer2 = Encoding.ASCII.GetBytes(msg2);
-
-                client1.Send(buffer2);
-                client2.Send(buffer1);
             }
-            n++;
+                n++;
 
         }
     }
-}
+} 
